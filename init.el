69af409916ac1ed76f1f2b9fb56331cd031e8dca
git <git@github.com>
2021-09-28 18:42:02 +0200

    push 2021 version

--- init.el
+++ init.el
@@ -1,4 +1,43 @@
-;;; EMACS CONFIG ;;;
+;; General Settings
+(setq default-directory "~/.emacs.d/files")
+(setq default-input-method "nil")
+
+(require 'simpleclip)
+
+(simpleclip-mode 1)
+
+;; Makes *scratch* empty.
+(setq initial-scratch-message "")
+
+;; Removes *scratch* from buffer after the mode has been set.
+(defun remove-scratch-buffer ()
+  (if (get-buffer "*scratch*")
+      (kill-buffer "*scratch*")))
+(add-hook 'after-change-major-mode-hook 'remove-scratch-buffer)
+
+;; Removes *messages* from the buffer.
+(setq-default message-log-max nil)
+(kill-buffer "*Messages*")
+
+;; Removes *Completions* from buffer after you've opened a file.
+(add-hook 'minibuffer-exit-hook
+      '(lambda ()
+         (let ((buffer "*Completions*"))
+           (and (get-buffer buffer)
+                (kill-buffer buffer)))))
+
+;; Don't show *Buffer list* when opening multiple files at the same time.
+(setq inhibit-startup-buffer-menu t)
+
+;; Show only one active window when opening multiple files at the same time.
+(add-hook 'window-setup-hook 'delete-other-windows)
+
+;; Transparency ;; 
+ ;;(set-frame-parameter (selected-frame) 'alpha '(<active> . <inactive>))
+ ;;(set-frame-parameter (selected-frame) 'alpha <both>)
+(set-frame-parameter (selected-frame) 'alpha '(95 . 100))
+(add-to-list 'default-frame-alist '(alpha . (95 . 100)))
+
 
 ;; init logging
 (message "Starting Emacs %s" emacs-version)
@@ -14,11 +53,23 @@
 (setq disabled-command-function nil)
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+;; reinstall all packages, useful for migrating to another pc
+(defun package-reinstall-all-activated-packages ()
+  "Refresh and reinstall all activated packages."
+  (interactive)
+  (package-refresh-contents)
+  (dolist (package-name package-activated-list)
+    (when (package-installed-p package-name)
+      (unless (ignore-errors                   ;some packages may fail to install
+                (package-reinstall package-name))
+        (warn "Package %s failed to reinstall" package-name)))))
+
 ;; prefer UTF-8
 (prefer-coding-system 'utf-8)
 
 ;; Disable the splash screen (to enable it agin, replace the t with 0)
 (setq inhibit-splash-screen 0)
+
 
 ;; Enable transient mark mode
 (transient-mark-mode 1)
@@ -27,9 +78,9 @@
 (line-number-mode 1)
 
 ;; reinit last session
-(desktop-save-mode 1)
-(savehist-mode 1)
-(add-to-list 'savehist-additional-variables 'kill-ring) ;; for example
+;(desktop-save-mode 0)
+;(savehist-mode 0)
+;(add-to-list 'savehist-additional-variables 'kill-ring) ;; for example
 
 ;; Start fullscreen (cross-platf)
 ;(add-hook 'window-setup-hook 'toggle-frame-fullscreen t)
@@ -38,21 +89,24 @@
 (menu-bar-mode -1)
 
 ;; Enable Powershell
-(require 'powershell)
+;; (require 'powershell)
 
 ;; Enable Ido Mode
 (ido-mode 1)
 
 
 ;;; AUTOSTART ;;;
-(add-hook 'after-init-hook 'org-todo-list)
-(add-hook 'after-init-hook 'powerline-center-theme)
-
+;(add-hook 'after-init-hook 'org-todo-list)
+;(add-hook 'after-init-hook 'powerline-center-theme)
+(add-hook 'after-init-hook (lambda () (org-agenda nil "z")))
 
 ;;; SHORTCUTS ;;;
 ;; org-agenda shortcut (C a t)
 (global-set-key "\C-ca" 'org-agenda)
-
+(global-set-key (kbd "C-c <left>")  'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>")    'windmove-up)
+(global-set-key (kbd "C-c <down>")  'windmove-down)
 
 
 ;;; THEMES ;;;
@@ -74,7 +128,7 @@ There are two things you can do about this warning:
 2. Remove this warning from your init file so you won't see it again."))
   ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
     (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
@@ -86,7 +140,7 @@ There are two things you can do about this warning:
   (interactive "f")
   (let ((base64-string
 	 (with-temp-buffer
-	   (insert-file-contents-literally filename)
+R	   (insert-file-contents-literally filename)
 	   (base64-encode-region (point-min) (point-max))
 	   (buffer-string))))
 	(insert (format "#+BEGIN_SRC emacs-lisp :results output silent\n  (with-temp-file %S\n    (insert (base64-decode-string\n      %S)))\n#+END_SRC" filename base64-string))))
@@ -101,9 +155,9 @@ There are two things you can do about this warning:
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 
 (setq org-todo-keywords
-  '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
+  '((sequence "FUTURE" "TODO" "NEXT" "IN-PROGRESS" "WAITING" "DONE")))
 
-(setq org-tag-alist '(("@work" . ?w) ("@home" . ?h) ("study" . ?l)))
+(setq org-tag-alist '(("@work" . ?w) ("@home" . ?h) ("@study" . ?s)))
 
 ;; Files belonging to org-mode Agenda
 (custom-set-variables
@@ -113,16 +167,13 @@ There are two things you can do about this warning:
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
- '(custom-enabled-themes (quote (grandshell)))
+ '(auth-source-save-behavior nil)
+ '(custom-enabled-themes '(grandshell))
  '(custom-safe-themes
-   (quote
-    ("3860a842e0bf585df9e5785e06d600a86e8b605e5cc0b74320dfe667bcbe816c" "6d19d236838fd93e73c66718d91a4f6ffb57223e8d1c1fbd19879190b3b6f7fa" default)))
- '(org-agenda-files
-   (quote
-    ("c:/Users/jd/Dropbox/Files/work.org" "c:/Users/jd/Dropbox/Files/study.org" "c:/Users/jd/Dropbox/Files/portfolio.org" "~/.org-jira/SC.org" "~/.org-jira/OIS.org" "~/.org-jira/ELSS.org")))
+   '("3e335d794ed3030fefd0dbd7ff2d3555e29481fe4bbb0106ea11c660d6001767" "cc0dbb53a10215b696d391a90de635ba1699072745bf653b53774706999208e3" "3860a842e0bf585df9e5785e06d600a86e8b605e5cc0b74320dfe667bcbe816c" "039c01abb72985a21f4423dd480ddb998c57d665687786abd4e16c71128ef6ad" default))
+ '(org-agenda-files '("~/.emacs.d/files/"))
  '(package-selected-packages
-   (quote
-    (pcre2el org-super-agenda twittering-mode ## elscreen centaur-tabs helm-smex smex powerline grandshell-theme howdoi org-cliplink org-jira org-edna)))
+   '(minimal-theme leuven-theme docker simpleclip sudo-edit py-autopep8 jedi elpy powershell bug-hunter pcre2el org-super-agenda twittering-mode ## elscreen centaur-tabs powerline grandshell-theme howdoi org-cliplink org-jira org-edna))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -135,27 +186,21 @@ There are two things you can do about this warning:
 ;;; REGEX Builder ;;;
 (defun pcre-regexp-from-list-of-words (words)
   "insert a pcre regexp to match a list of words"
-  (interactive "sList of words for regexp: ")
+  (interactive "List of words for regexp: ")
   (insert
    (pcre-to-elisp
     (regexp-opt (split-string words)))))
 (global-set-key (kbd "C-c R") 'pcre-regexp-from-list-of-words)
-
 
 ;;; ORG-SUPER-AGENDA ;;;
 (require 'org-super-agenda)
 (org-super-agenda-mode t)
 
 
-(setq spacemacs-theme-org-agenda-height nil
-      org-agenda-time-grid '((daily today require-timed) "----------------------" nil)
-      org-agenda-skip-scheduled-if-done t
-      org-agenda-skip-deadline-if-done t
-      org-agenda-include-deadlines t
-      org-agenda-include-diary t
+(setq org-agenda-include-deadlines t
       org-agenda-block-separator nil
-      org-agenda-compact-blocks t
-      org-agenda-start-with-log-mode t)
+      org-agenda-compact-blocks t)
+
 
 (setq org-agenda-custom-commands
       '(("z" "Super zaen view"
@@ -167,64 +212,49 @@ There are two things you can do about this warning:
                                 :todo "TODAY"
                                 :scheduled today
                                 :order 1)))))
+	  
           (alltodo "" ((org-agenda-overriding-header "")
                        (org-super-agenda-groups
                         '(
 
-			
-			  
-			  (:name "Study"
-                                 :tag ("Study" "Daily")
-                                 :order 1)
 
 			  (:name "Overdue"
                                  :deadline past			      
-                                 :order 2)			  
-
+                                 :order 10)			  
 			  (:name "Due Today"
                                  :deadline today
-                                 :order 3)
-                          (:name "Due Soon"
-                                 :deadline future
-                                 :order 4)
-
-			 
-                          (:name "Personal Projects"
-                                 :tag "Projects"
-                                 :order 9)
-                          (:name "Emacs"
-                                 :tag "Emacs"
-                                 :order 10)
-                          (:name "To read"
-                                 :tag "Read"
                                  :order 11)
+			  (:name "Due Soon"
+				 :tag "todo"
+                                 :deadline future
+                                 :order 12)
 
+                          (:name "Projects"
+                                 :tag "projects"
+                                 :order 20)
+                          (:name "To read"
+                                 :tag "read"
+                                 :order 21)
+                          (:name "Games"
+                                 :tag "games"
+                                 :order 22)
 					  
-			   (:name "Work"
+			  (:name "Work"
                                  :tag "work"
-                                 :order 5)
-			  
-			  (:name "Orpheus_SC"
-                                 :regexp "SC_"
-                                 :order 6)
-			  (:name "Orpheus_ELSS"
-                                 :regexp "ELSS_"
-                                 :order 7)
-		          (:name "Orpheus_OIS"
-                                 :regexp "OIS_"
-                                 :order 8)
+                                 :order 30)
 
-			  
-			   (:name "Exam Dates"
+			  (:name "Exam Dates"
 				 :tag "ExamDates"
                                  :order 99)
 			  
-                          (:discard (:tag ("Chore" "Routine" "Daily")))))))))))
+                          (:discard (:tag ("weekly")))))))))))
+
+
 
 
 ;;; SMEX ;;;
-  (require 'smex) ; Not needed if you use package.el
-  (smex-initialize) ; Can be omitted. This might cause a (minimal) delay
+;  (require 'smex) ; Not needed if you use package.el
+;  (smex-initialize) ; Can be omitted. This might cause a (minimal) delay
 	            ; when Smex is auto-initialized on its first run.
 ;;Key Bindings;;
   (global-set-key (kbd "M-x") 'smex)
@@ -236,7 +266,10 @@ There are two things you can do about this warning:
 (elscreen-start)
 
 ;;; org-jira ;;;
-(setq jiralib-url "https://orpheus.jira.com")
-(require 'org-jira)
+;(setq jiralib-url "https://orpheus.jira.com")
+;(require 'org-jira)
+;(setq org-jira-working-dir "~/.emacs.d/files/org-jira")
 
-
+;;; elpy ;;:
+(elpy-enable)
+;;;;
